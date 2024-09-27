bf12e96e58c9912d3da6c5b355edc34d4cdf235f
Han <1655466387@qq.com>
2024-09-27 10:42:36 +0800

    优化统计代码

--- public/tracker.js
+++ public/tracker.js
@@ -1,4 +1,4 @@
-(window => {
+((window) => {
   const { location, document, navigator } = window;
   const { hostname: host, pathname } = location;
   const { currentScript, referrer } = document;
@@ -11,24 +11,41 @@
   let visit = true;
   // 访问间隔 (分钟)
   const AccessInterval = 30;
-  const vhLastVisitT = Number(localStorage.getItem("_vhLastVisitT")) || 0;
+  // 页面访问
+  let PathVisit = {};
+  try {
+    PathVisit = JSON.parse(localStorage.getItem("_vhLastVisit")) || {};
+    if (typeof PathVisit != "object") PathVisit = {};
+  } catch (error) {
+    localStorage.removeItem("_vhLastVisit");
+  }
+  const vhLastVisit = PathVisit[pathname] || 0;
+  // 访客访问
+  const vhLastVisitor = localStorage.getItem("_vhLastVisitor") || 0;
   // 是否是今天缓存
   const nowVisitDate = new Date();
-  const vhLastVisitDate = new Date(vhLastVisitT);
-  if (nowVisitDate.getFullYear() === vhLastVisitDate.getFullYear() && nowVisitDate.getMonth() === vhLastVisitDate.getMonth() && nowVisitDate.getDate() === vhLastVisitDate.getDate()) {
+  const vhLastVisitorDate = new Date(Number(vhLastVisitor));
+  const vhLastVisitDate = new Date(Number(vhLastVisit));
+  // 访客访问
+  if (nowVisitDate.getFullYear() === vhLastVisitorDate.getFullYear() && nowVisitDate.getMonth() === vhLastVisitorDate.getMonth() && nowVisitDate.getDate() === vhLastVisitorDate.getDate()) {
     // 今天缓存即老用户
     visitor = false;
+  } else {
+    // 否则更新缓存时间
+    localStorage.setItem("_vhLastVisitor", Date.now());
   }
+  // 访问次数访问
   if (Date.now() - vhLastVisitDate.getTime() < AccessInterval * 60 * 1000) {
     // 超过访问间隔即老访客
     visit = false;
   } else {
     // 否则更新缓存时间
-    localStorage.setItem("_vhLastVisitT", Date.now());
+    PathVisit[pathname] = Date.now();
+    localStorage.setItem("_vhLastVisit", JSON.stringify(PathVisit));
   }
 
   // URL转码
-  const encode = str => {
+  const encode = (str) => {
     if (!str) return "";
     try {
       if (decodeURI(str) !== str) return decodeURI(str);
